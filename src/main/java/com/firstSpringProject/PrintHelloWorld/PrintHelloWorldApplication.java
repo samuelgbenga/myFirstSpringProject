c9be271ed268c72b804689c4ee49146b9503e11f
samuelgbenga <samuelgbenga972@gmail.com>
2024-05-28 05:27:12 +0100

    first spring project

---
+++ src/main/java/com/firstSpringProject/PrintHelloWorld/PrintHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.firstSpringProject.PrintHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstSpringProject/PrintHelloWorld/PrintHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstSpringProject.PrintHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
